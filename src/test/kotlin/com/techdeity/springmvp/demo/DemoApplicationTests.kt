208fa5c443fa4d46a31bd69285149eccd76efb65
Vishal <31177415+tech-deity@users.noreply.github.com>
2023-02-15 11:55:01 +0530

    Initial commit

---
+++ src/test/kotlin/com/techdeity/springmvp/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techdeity.springmvp.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
